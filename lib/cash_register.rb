8c39f29c220168a0d0f63641aa3cc72418294d30
Levi Borland <borland.levi@gmail.com>
2019-09-10 05:30:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  
+  
+  attr_accessor :total, :discount, :items, :last_transaction_amount
 
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += (price * quantity)
+    @last_transaction_amount = (price * quantity)
+  end
+  
+
+
+
+end
