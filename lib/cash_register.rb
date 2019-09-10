bc65571263a34105b088b6214ed4ca1711410705
Levi Borland <borland.levi@gmail.com>
2019-09-10 06:12:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,10 +13,19 @@ class CashRegister
     @total
   end
   
-  def add_item(title, price, quantity = 1)
-    @total += (price * quantity)
-    @last_transaction_amount = (price * quantity)
-    @items << title
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
   end
   
   def apply_discount
